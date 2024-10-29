958e2e9ea088dfa589413678a5d8206e27b212cf
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-10-29 14:43:17 -0300

    ft: added get in equestMatchers

--- src/main/java/com/halloween/config/SecurityConfig.java
+++ src/main/java/com/halloween/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,7 +36,9 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/auth/**")
+                        req.requestMatchers(HttpMethod.GET, "/api/stories/**")
+                                .permitAll()
+                                .requestMatchers("/auth/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
